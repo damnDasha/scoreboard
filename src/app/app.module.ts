a85a6a29341363bea0059c629af7526b9e8e05ce
dasha akhten <dasha.akhten@gmail.com>
2021-10-28 20:11:23 -0700

    fix: no more aboutme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,13 +14,9 @@ import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AddTaskComponent } from './components/add-task/add-task.component';
-import { AboutComponent } from './components/about/about.component';
 import { FooterComponent } from './components/footer/footer.component';
 
-const appRoutes: Routes = [
-  { path: '', component: TasksComponent },
-  { path: 'about', component: AboutComponent },
-];
+const appRoutes: Routes = [{ path: '', component: TasksComponent }];
 
 @NgModule({
   declarations: [
